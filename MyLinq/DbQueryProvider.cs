d7fbafa7e0389dd52f4b33457c9526081e134488
X5056 <X5056@winton.com.tw>
2013-12-19 15:23:00 +0800

    MyLinq完成到Part2

---
+++ MyLinq/DbQueryProvider.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLinq
+{
+	public class DbQueryProvider : QueryProvider
+	{
+		DbConnection connection;
+
+		public DbQueryProvider(DbConnection connection)
+		{
+			this.connection = connection;
+		}
+
+		public override string GetQueryText(Expression expression)
+		{
+			return this.Translate(expression);
+		}
+
+		public override object Execute(Expression expression)
+		{
+			DbCommand cmd = this.connection.CreateCommand();
+			cmd.CommandText = this.Translate(expression);
+			DbDataReader reader = cmd.ExecuteReader();
+			Type elementType = TypeSystem.GetElementType(expression.Type);
+			return Activator.CreateInstance(
+				typeof(ObjectReader<>).MakeGenericType(elementType),
+				BindingFlags.Instance | BindingFlags.NonPublic, null,
+				new object[] { reader },
+				null);
+		}
+
+		private string Translate(Expression expression)
+		{
+			return new QueryTranslator().Translate(expression);
+		}
+	}
+}
---
+++ MyLinq/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLinq
+{
+	public class Customers
+	{
+		public string CustomerID;
+		public string ContactName;
+		public string Phone;
+		public string City;
+		public string Country;
+	}
+
+	public class Orders
+	{
+		public int OrderID;
+		public string CustomerID;
+		public DateTime OrderDate;
+	}
+
+	public class Northwind
+	{
+		public Query<Customers> Customers;
+		public Query<Orders> Orders;
+
+		public Northwind(DbConnection connection)
+		{
+			QueryProvider provider = new DbQueryProvider(connection);
+			this.Customers = new Query<Customers>(provider);
+			this.Orders = new Query<Orders>(provider);
+		}
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string constr = @"Data Source=.;Initial Catalog=Northwind;Integrated Security=True;Connect Timeout=15;Encrypt=False;TrustServerCertificate=False";
+			using (SqlConnection con = new SqlConnection(constr))
+			{
+				con.Open();
+				Northwind db = new Northwind(con);
+
+				IQueryable<Customers> query =
+					 db.Customers.Where(c => c.City == "London");
+
+				Console.WriteLine("Query:\n{0}\n", query);
+
+				var list = query.ToList();
+				foreach (var item in list)
+				{
+					Console.WriteLine("Name: {0}", item.ContactName);
+				}
+
+				Console.ReadLine();
+			}
+		}
+	}
+}
