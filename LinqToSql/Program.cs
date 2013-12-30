dba0441bf4d161cc7a17d0fdda325d08fe3656cf
X5056 <X5056@winton.com.tw>
2013-12-30 16:38:46 +0800

    测试

---
+++ LinqToSql/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqToSql
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var qp = new QueryProvider();
+			var q = new Query<string>(qp);
+
+			var result = q.Where(o => 1 == 1);
+			foreach (var r in result)
+			{
+				Console.WriteLine(r);
+			}
+		}
+	}
+	class Query<T> : IQueryable<T>
+	{
+		public IQueryProvider Provider
+		{
+			get;
+			private set;
+		}
+		public Expression Expression
+		{
+			get;
+			private set;
+		}
+		public Type ElementType
+		{
+			get { return typeof(T); }
+		}
+
+		public Query(IQueryProvider provider)
+		{
+			Provider = provider;
+			Expression = Expression.Constant(this);
+		}
+
+		public Query(IQueryProvider provider, Expression express)
+		{
+			Provider = provider;
+			Expression = express;
+		}
+		public IEnumerator<T> GetEnumerator()
+		{
+			return Provider.Execute<T>(Expression) as IEnumerator<T>;
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+
+	class QueryProvider : IQueryProvider
+	{
+		public IQueryable<TElement> CreateQuery<TElement>(Expression expression)
+		{
+			return new Query<TElement>(this, expression);
+		}
+
+		public IQueryable CreateQuery(Expression expression)
+		{
+			throw new NotImplementedException();
+		}
+
+		public TResult Execute<TResult>(Expression expression)
+		{
+			var l = Expression.Lambda(expression).Compile();
+			return (TResult)l.DynamicInvoke();
+		}
+
+		public object Execute(Expression expression)
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+
+}
--- MyLinq/Program.cs
+++ MyLinq/Program.cs
@@ -41,6 +41,8 @@ namespace MyLinq
 	{
 		static void Main(string[] args)
 		{
+			LinqTest();
+			return;
 			string constr = @"Data Source=.;Initial Catalog=Northwind;Integrated Security=True;Connect Timeout=15;Encrypt=False;TrustServerCertificate=False";
 			using (SqlConnection con = new SqlConnection(constr))
 			{
@@ -71,5 +73,51 @@ namespace MyLinq
 				Console.ReadLine();
 			}
 		}
+
+		static void LinqTest()
+		{
+			var group = typeof(string).GetMembers().GroupBy(o => o.MemberType);
+			foreach (var g in group)
+			{
+				Console.WriteLine(g.Key);
+				foreach (var gg in g)
+				{
+					Console.WriteLine("\t" + gg.Name);
+				}
+			}
+			//--------------------
+			List<Package> packages = new List<Package> { new Package { Company = "Coho Vineyard", Weight = 25.2, TrackingNumber = 89453312L },
+                                                 new Package { Company = "Lucerne Publishing", Weight = 18.7, TrackingNumber = 89112755L },
+                                                 new Package { Company = "Wingtip Toys", Weight = 6.0, TrackingNumber = 299456122L },
+                                                 new Package { Company = "Contoso Pharmaceuticals", Weight = 9.3, TrackingNumber = 670053128L },
+                                                 new Package { Company = "Wide World Importers", Weight = 33.8, TrackingNumber = 4665518773L } };
+
+			ILookup<char, string> packageLookup = packages.ToLookup(
+				p => Convert.ToChar(p.Company.Substring(0, 1)),
+				p => p.Company + " " + p.TrackingNumber
+				);
+
+			foreach (var packageGroup in packageLookup)
+			{
+				Console.WriteLine(packageGroup.Key);
+				foreach (string str in packageGroup)
+					Console.WriteLine("    {0}", str);
+			}
+			//----------------------------
+			string[] fruits = { "apricot", "orange", "banana", "mango", "apple", "grape", "strawberry" };
+
+			IOrderedEnumerable<string> sortedFruits1 =
+				fruits.OrderBy(fruit => fruit.Length).ThenBy(fruit => fruit);
+
+			foreach (string fruit in sortedFruits1)
+				Console.WriteLine(fruit);
+
+		}
+	}
+	class Package
+	{
+		public string Company { get; set; }
+		public double Weight { get; set; }
+		public long TrackingNumber { get; set; }
 	}
 }
